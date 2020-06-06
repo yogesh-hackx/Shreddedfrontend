137aa7b3c106b3c484b1c7e5f7a7bb594de0ad30
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-07 00:41:51 +0530

    fixed jumbotron

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Navbar from '../components/Reusable/Navbar/Navbar'
-import Jumbotron from '../components/Reusable/Jumbotron/Jumbotron'
-import Footer from '../components/Reusable/Footer/Footer'
-import CardEnroll from '../components/Reusable/CardEnroll/CardEnroll'
+import Navbar from '../components/Reusable/Navbar/Navbar';
+import Jumbotron from '../components/Reusable/Jumbotron/Jumbotron';
+import Footer from '../components/Reusable/Footer/Footer';
+import CardEnroll from '../components/Reusable/CardEnroll/CardEnroll';
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Jumbotron />
-      <CardEnroll />
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <Jumbotron />
+            <CardEnroll />
+            <Footer />
+        </>
+    );
 }
