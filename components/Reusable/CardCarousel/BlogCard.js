4c9fd5a2a3140f310f1755f0c5e27597a69c3d13
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-07 10:18:00 +0530

    Added blog card carousel

---
+++ components/Reusable/CardCarousel/BlogCard.js
@@ -0,0 +1,26 @@
+import styles from './BlogCard.module.css'
+
+const BlogCard = (props) => {
+    return (
+        <>
+            <div className={styles.card}>
+                <div className={styles.coverContainer}>
+                    <img
+                        src="/assets/sampleCover.png"
+                        alt="coverImage"
+                        className={styles.coverImage}
+                    />
+                </div>
+                <div className={styles.blogHeadline}>Headline here</div>
+                <div className={styles.blogEllipsis}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    odit voluptatum numquamamet consectetur adipisicing elit.
+                    deleniti.
+                </div>
+                <div className={styles.readMore}>READ MORE</div>
+            </div>
+        </>
+    );
+};
+
+export default BlogCard;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Navbar from '../components/Reusable/Navbar/Navbar';
 import Jumbotron from '../components/Reusable/Jumbotron/Jumbotron';
 import Footer from '../components/Reusable/Footer/Footer';
 import CardContainer from '../components/Reusable/CardEnroll/CardContainer';
+import CardCarousel from '../components/Reusable/CardCarousel/CardCarousel';
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <Jumbotron />
+            <CardCarousel />
             <CardContainer />
             <Footer />
         </>
