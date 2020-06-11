0a3a4a932582bdd0b06f4670dbf7d7881da4f326
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-11 14:15:39 +0530

    minor fixes

--- components/Reusable/CardBlog/CardBlog.jsx
+++ components/Reusable/CardBlog/CardBlog.jsx
@@ -1,20 +1,29 @@
-import styles from './CardBlog.module.css'
+import Link from 'next/link';
+import styles from './CardBlog.module.css';
 
 const CardBlog = (props) => {
     return (
         <div className={styles.cbBack}>
-            <div className={styles.cbMain}>
-                <img height="200px" width="300px" className={styles.cbImg} src={props.image} alt="img" />
-                <div className={styles.cbContent}>
-                    <h3 className={styles.cbTitle}>{props.title}</h3>
-                    <p className={styles.cbDes}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam labore nam itaque corporis,
-                        voluptatibus assumenda sint sunt! Soluta,
-                    eum! Quis libero culpa magnam corporis sint! Corrupti sed voluptatum nam amet.</p>
-                    <button className={styles.cbBtn}>Read more</button>
+            <Link href={`/blogs/${props.link}`}>
+                <div className={styles.cbMain}>
+                    <img
+                        height="200px"
+                        width="300px"
+                        className={styles.cbImg}
+                        src={props.image}
+                        alt="img"
+                    />
+                    <div className={styles.cbContent}>
+                        <h3 className={styles.cbTitle}>{props.title}</h3>
+                        <p className={styles.cbDes}>{props.des}</p>
+                        <Link href={`/blogs/${props.link}`}>
+                            <button className={styles.cbBtn}>Read more</button>
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
-    )
-}
+    );
+};
 
-export default CardBlog
+export default CardBlog;
