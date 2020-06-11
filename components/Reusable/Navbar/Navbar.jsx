d43089edd956021097efa44625ec37fcc2438bc3
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-11 14:11:28 +0530

    Added progress bar

--- components/Reusable/Navbar/Navbar.jsx
+++ components/Reusable/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import styles from './Navbar.module.css'
+import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import styles from './Navbar.module.css';
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 const Navbar = () => {
     return (
         <div className={styles.nbMain}>
-            <span className={styles.nbLogo}>SK</span>
-            <span className={styles.nbList}>BLOG</span>
-            <span className={styles.nbList}>PLANS</span>
-        </div >
-    )
-}
+            <Link href="/">
+                <span className={styles.nbLogo}>SK</span>
+            </Link>
+            <Link href="/blogs">
+                <span className={styles.nbList}>BLOG</span>
+            </Link>
+            <Link href="/plans">
+                <span className={styles.nbList}>PLANS</span>
+            </Link>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
