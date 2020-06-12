b43b8ec5b7eb3f7bf3f5e54fb9d3e11b51a4821f
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-12 15:21:13 +0530

    replaced nav buttons with muiButtons

--- components/Reusable/Navbar/Navbar.jsx
+++ components/Reusable/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Router from 'next/router';
+import Button from '@material-ui/core/Button';
 import NProgress from 'nprogress';
 import styles from './Navbar.module.css';
 
@@ -10,17 +11,28 @@ Router.onRouteChangeError = () => NProgress.done();
 const Navbar = () => {
     return (
         <div className={styles.nbMain}>
-            <Link href="/">
-                <span className={styles.nbLogo}>SK</span>
-            </Link>
-            <Link href="/blogs">
-                <span className={styles.nbList}>BLOG</span>
-            </Link>
-            <Link href="/plans">
-                <span className={styles.nbList}>PLANS</span>
-            </Link>
+            <div className={styles.nbLogo}>
+                <Link href="/">
+                    <Button style={logoStyles}>SK</Button>
+                </Link>
+            </div>
+            <div className={styles.nbList}>
+                <div className={styles.navItem}>
+                    <Link href="/plans">
+                        <Button style={NavButtonStyles}>Plans</Button>
+                    </Link>
+                </div>
+                <div className={styles.navItem}>
+                    <Link href="/blogs">
+                        <Button style={NavButtonStyles}>Blog</Button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
+
+const NavButtonStyles = { fontSize: '18px', color: '#fff' };
+const logoStyles = { fontSize: '20px', color: '#fff', fontWeight: '900px' };
 
 export default Navbar;
